19a7af28ecef34d540ef4aef65e9d3831d92c471
afnscbrl <afns.cbrl@gmail.com>
2022-06-06 16:07:55 -0300

    themes

---
+++ src/themes/NavTheme.ts
@@ -0,0 +1,16 @@
+import { createTheme } from '@mui/material'
+
+export const NavTheme = createTheme({
+    palette: {
+        primary: {
+            main: '#a33b12',
+        },
+        secondary: {
+            main: '#e9dac9'
+        },
+        background: {
+            default: '#e9dac9',
+            paper: '#d66f43',
+        }
+    }
+})
