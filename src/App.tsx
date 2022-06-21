d195640783cabd3b7b9810c628d0afebf3e4d5aa
afnscbrl <afns.cbrl@gmail.com>
2022-06-21 11:53:31 -0300

    SignIn page route added

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import About from './components/About';
 import Aulas from './components/Aulas'
 import Drawer from './components/Drawer'
 import Login from './components/Login';
+import Depo from './components/Depo';
+import SignIn from './components/SignIn'
 import './App.css';
 // import { Parallax, ParallaxProvider } from 'react-scroll-parallax';
 import { NavTheme } from './themes';
@@ -25,6 +27,8 @@ function App() {
           <Route path='/Inicio' element={<About/>} />
           <Route path='/Aulas' element={<Aulas/>}/>
           <Route path='/Login' element={<Login/>} />
+          <Route path='/Registrar' element={<SignIn/>} />
+          <Route path='/Depoimentos' element={<Depo/>} />
         </Routes>
 
       </Router>
