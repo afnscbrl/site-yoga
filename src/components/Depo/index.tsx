cd08b8f7f024f783791fa9dc1a5e074fd8c7b326
afnscbrl <afns.cbrl@gmail.com>
2022-06-25 09:24:24 -0300

    Depo page added

--- src/components/Depo/index.tsx
+++ src/components/Depo/index.tsx
@@ -1,11 +1,21 @@
 import { Box } from '@mui/material';
+import CardDepo from './CardDepo'
 import * as React from 'react';
+import './Depo.css'
 
 export default function Depo() {
-    return(
-        <Box className='Depoimentos'>
-            <h2 id='depoimentos'> Depoimentos </h2>
-            
+
+    return (
+        <Box className='conteudo__depos'>
+            <h2 className='depos__h2'> Depoimentos </h2>
+            <div className='cards_depo'>
+                <CardDepo aluna='aluna1' previa='previa1' depoimento='depoimento1'/>
+                <CardDepo aluna='aluna2' previa='previa2' depoimento='depoimento2'/>
+                <CardDepo aluna='aluna1' previa='previa1' depoimento='depoimento1'/>
+                <CardDepo aluna='aluna2' previa='previa2' depoimento='depoimento2'/>
+                <CardDepo aluna='aluna1' previa='previa1' depoimento='depoimento1'/>
+                <CardDepo aluna='aluna2' previa='previa2' depoimento='depoimento2'/>
+            </div>
         </Box>
     )
 }
