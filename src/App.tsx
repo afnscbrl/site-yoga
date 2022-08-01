1f3f7712179f9e1bb3ae21b43f03f33461f6ddcc
afnscbrl <afns.cbrl@gmail.com>
2022-08-01 19:22:12 -0300

    App refatored

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,19 @@
 import * as React from 'react';
-import Header from './components/Header';
-import About from './components/About';
-import Aulas from './components/Aulas'
-import Drawer from './components/Drawer'
-import Login from './components/Login';
-import Depo from './components/Depo';
-import SignIn from './components/SignIn'
-import Contato from './components/Email'
-import Anamnese from './components/Anamnese'
-import './App.css';
-// import { Parallax, ParallaxProvider } from 'react-scroll-parallax';
+import Routes from './routes'
+import { UserContextProvider } from './context/isAuth';
 import { NavTheme } from './themes';
 import { ThemeProvider } from '@mui/material';
-import { Route, BrowserRouter as Router, Routes, Navigate } from 'react-router-dom';
+import './App.css';
+
 
 function App() {
-
-
-
   return (
-
     <ThemeProvider theme={NavTheme}>
-      <Router>
-
-        <Header />
-        <Drawer />
-        <Routes>
-          <Route path='/' element={<About/>} />
-          <Route path='/Aulas' element={<Aulas/>}/>
-          <Route path='/Login' element={<Login/>} />
-          <Route path='/Registrar' element={<SignIn/>} />
-          <Route path='/Depoimentos' element={<Depo/>} />
-          <Route path='/Contato' element={<Contato/>} />
-          <Route path='/Inicio' element={<Navigate to='/'/>}/>
-          <Route path='/Anamnese' element={<Anamnese/>}/>
-        </Routes>
-
-      </Router>
+      <UserContextProvider>
+        <Routes/>
+      </UserContextProvider>
     </ThemeProvider>
-
-
-  );
+  )
 }
 
 export default App;
