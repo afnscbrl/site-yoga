2a92ce3cd98229ca2e9ee293ff2c16fca8b6f607
afnscbrl <afns.cbrl@gmail.com>
2022-08-26 05:53:36 -0300

    ...

--- src/components/Private/Aulas/AulasEdit/index.tsx
+++ src/components/Private/Aulas/AulasEdit/index.tsx
@@ -1,8 +1,10 @@
-import { Box, Button, List, ListItem, ListItemButton, Modal } from '@mui/material';
+import { Box, Button, FormControl, List, ListItem, ListItemButton, Modal, OutlinedInput, Typography } from '@mui/material';
 import axios from 'axios';
 import * as React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { userContext } from '../../../../context/isAuth';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import EditIcon from '@mui/icons-material/Edit';
 import "./AulasEdit.css"
 
 const style = {
@@ -10,51 +12,102 @@ const style = {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
+    width: '80vw',
+    bgcolor: '#e9dac9',
+    border: '2px solid #a33b12',
     boxShadow: 24,
     p: 4,
-  };
+};
 
 {/* <iframe src="https://drive.google.com/file/d/1xgjCjJA_-pzFPsRAgcV5rau2b4JSHuBc/preview" width="640" height="480" allow="autoplay"></iframe> */ }
 export function AulasEdit() {
     const [aulas, setAulas] = React.useState([])
-    const { categoria, setCategoria } = React.useContext(userContext)
+    const [aula, setAula] = React.useState({})
+    const [aulaId, setAulaId ] = React.useState('')
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
+    const { categoria, setCategoria } = React.useContext(userContext)
     const navigate = useNavigate()
+
     //Settin all axios requests with Authorization header
     axios.defaults.headers.common['Authorization'] = `${localStorage.getItem('Token')}`
 
     React.useEffect(() => {
-        axios.get(`${process.env.REACT_APP_LINK_HOST}aulas/${categoria}`)
-            .then(res => setAulas(res.data))
+        listaAulas()
+        
     }, [])
 
-    const clickAulas = (id: string) => {
-        // setAulaId(id)
-        navigate('/alunaedit')
-
+    const listaAulas = () => {
+        axios.get(`${process.env.REACT_APP_LINK_HOST}aulas/${categoria}`)
+        .then(res => setAulas(res.data))
+        //editar catch com mesagem de erro e redirecionamento
     }
 
+    const clickAula = (a: object) => {
+        setAulaId(Object.values(a)[0])
+        setAula(a)
+        handleOpen()
+    }
+
+    const deleteAula = (id: string) => {
+        try {
+            axios.delete(`${process.env.REACT_APP_LINK_HOST}aulas/${id}`)
+            listaAulas()
+        } catch (err) {
+            console.log(err)
+            //tratar erro com mesagem.
+        }
+    }
+
+    const changeForm = (event: React.ChangeEvent<HTMLInputElement>) => {
+        event.stopPropagation()
+        const campo = [[event.target.name, event.target.value]]
+        const novoValor = Object.fromEntries(campo)
+        const novaAula = { ...aula, ...novoValor }
+        setAula(novaAula)
+    }
+
+    const aulaEditarInput = async (event: { preventDefault: () => void, stopPropagation: () => void; }) => {
+
+        event.preventDefault()
+        event.stopPropagation()
+        try {
+            await axios.put(`${process.env.REACT_APP_LINK_HOST}aulas/${aulaId}`, aula)
+
+        } catch (err) {
+            console.log(err)
+            //Tratar err
+        }
+        listaAulas()
+        handleClose()
+    }
+    
     return (
 
-        <div className='conteudo__alunasList'>
-            <h2>Aulas</h2>
+        <div className='conteudo__aulasEditList'>
+            <h2>Aulas de {categoria}</h2>
             <List>
                 {
                     aulas ? aulas.map((e: object, i: number) =>
                         <ListItem disablePadding key={i.toString()} sx={{ marginTop: 1, marginBottom: 1, backgroundColor: '#a33b12', opacity: '0.7' }}>
-                            <ListItemButton sx={{ alignItems: 'center', color: 'white' }}
-                                onClick={() => clickAulas(Object.entries(e)[0][1])}>
-                                <div className='listItemText'>
-                                    <p ><b>Categoria:</b> {Object.entries(e)[1][1]}</p>
-                                    <p ><b>Link:</b> {Object.entries(e)[2][1]}</p>
-                                    <p ><b>Data:</b> {Object.entries(e)[3][1]}</p>
+                            <div className='listItemText_aulasEdit'>
+                                <p ><b>Data:</b> {Object.entries(e)[4][1]}</p>
+                                {/* editar o href para iframe */}
+                                <p ><b>Link:</b> <a href={Object.entries(e)[3][1]}>{Object.entries(e)[2][1]}</a></p>
+                                <div className='listButtons_aulasEdit'>
+                                    <ListItemButton
+                                        onClick={() => clickAula(e)}
+                                        sx={{ justifyContent: 'flex-end' }}>
+                                        <EditIcon />
+                                    </ListItemButton>
+                                    <ListItemButton
+                                        onClick={() => deleteAula(Object.entries(e)[0][1])}
+                                        sx={{ justifyContent: 'flex-end' }}>
+                                        <DeleteForeverIcon />
+                                    </ListItemButton>
                                 </div>
-                            </ListItemButton>
+                            </div>
                         </ListItem>
                     ) :
                         <ListItem >
@@ -64,8 +117,6 @@ export function AulasEdit() {
             </List >
             <div className='button__alunaEdit'>
                 <Button onClick={() => navigate('/aulasadm')}>Voltar</Button>
-                <Button onClick={handleOpen}>Criar</Button>
-
             </div>
             <Modal
                 open={open}
@@ -74,8 +125,39 @@ export function AulasEdit() {
                 aria-describedby="modal-modal-description"
                 className="aulasEdit_modal"
             >
-                <Box  sx={style}>
-                <p>TEste</p>
+                <Box sx={style}>
+                    <form onSubmit={aulaEditarInput}>
+                        <Typography sx={{ marginLeft: '10px', marginRight: '10px' }}>
+                            Categoria:
+                        </Typography>
+                        <FormControl fullWidth onChange={changeForm} sx={{ backgroundColor: '#e9dac9' }}>
+                            <OutlinedInput name="categoria" size='small'  value={categoria}/>
+                        </FormControl>
+                        <Typography sx={{ marginLeft: '10px', marginRight: '10px' }}>
+                            Descricao:
+                        </Typography>
+                        <FormControl fullWidth onChange={changeForm} sx={{ backgroundColor: '#e9dac9' }}>
+                            <OutlinedInput name="descricao" size='small' defaultValue={Object.values(aula)[2]} />
+                        </FormControl>
+                        <Typography sx={{ marginLeft: '10px', marginRight: '10px' }}>
+                            Link:
+                        </Typography>
+                        <FormControl fullWidth onChange={changeForm} sx={{ backgroundColor: '#e9dac9' }}>
+                            <OutlinedInput name="link" size='small'  defaultValue={Object.values(aula)[3]}/>
+                        </FormControl>
+                        <Typography sx={{ marginLeft: '10px', marginRight: '10px' }}>
+                            Data:
+                        </Typography>
+                        <FormControl fullWidth onChange={changeForm} sx={{ backgroundColor: '#e9dac9' }}>
+                            <OutlinedInput name="data" size='small' defaultValue={Object.values(aula)[4]}/>
+                        </FormControl>
+                        <Button onClick={handleClose}>
+                            Cancelar
+                        </Button>
+                        <Button type="submit">
+                            Editar
+                        </Button>
+                    </form>
                 </Box>
             </Modal>
 
--- src/components/Private/Aulas/index.tsx
+++ src/components/Private/Aulas/index.tsx
@@ -1,41 +1,83 @@
-import { List, ListItem, ListItemButton } from '@mui/material';
+import { Box, Button, FormControl, List, ListItem, ListItemButton, Modal, OutlinedInput, Typography } from '@mui/material';
 import axios from 'axios';
 import * as React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { userContext } from '../../../context/isAuth';
+import "./Aulas.css"
+
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '80vw',
+    bgcolor: '#e9dac9',
+    border: '2px solid #a33b12',
+    boxShadow: 24,
+    p: 4,
+};
 
 export function Aulas() {
     let cats: Array<string> = []
     const [aulas, setAulas] = React.useState([])
-    const {categoria, setCategoria} = React.useContext(userContext)
+    const [aula, setAula] = React.useState([])
+    const { categoria, setCategoria } = React.useContext(userContext)
     const navigate = useNavigate()
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
     //Settin all axios requests with Authorization header
     axios.defaults.headers.common['Authorization'] = `${localStorage.getItem('Token')}`
 
     React.useEffect(() => {
         axios.get(`${process.env.REACT_APP_LINK_HOST}aulas`)
-            .then(res => setAulas(res.data))
-    }, [])
+            .then(res => {
+                setAulas(res.data)
+            })
+    }, [categoria])
 
     const clickCat = (nome: string) => {
-        setCategoria(nome)
+        setCategoria(nome.toUpperCase())
         navigate('/aulasedit')
+    }
 
+    const changeForm = (event: React.ChangeEvent<HTMLInputElement>) => {
+        event.stopPropagation()
+        const campo = [[event.target.name, event.target.value]]
+        const novoValor = Object.fromEntries(campo)
+        const novaAula = { ...aula, ...novoValor }
+        setAula(novaAula)
+    }
+
+    const aulaCriarInput = async (event: { preventDefault: () => void, stopPropagation: () => void; }) => {
+
+        event.preventDefault()
+        event.stopPropagation()
+        try {
+            await axios.post(`${process.env.REACT_APP_LINK_HOST}aulas`, aula)
+
+        } catch (err) {
+            console.log(err)
+            //Tratar err
+        }
+        clickCat(Object.values(aula)[0])
     }
 
     if (aulas) {
-        aulas.map(e => {
-                if (!cats.includes(Object.values<string>(e)[1])) {
-                    cats.push(Object.values<string>(e)[1])
-                }
-            })
+        aulas.map((e: string) => {
+            if (!cats.includes(Object.values<string>(e)[1])) {
+                cats.push(Object.values<string>(e)[1])
+            }
+        })
     }
 
 
     return (
 
-        <div className='conteudo__alunasList'>
+        <div className='conteudo__aulasList'>
             <h2>Aulas</h2>
+            <h5>ESCOLHA A CATEGORIA</h5>
             <List>
                 {
                     cats ? cats.map((e: string, i: number) =>
@@ -53,6 +95,51 @@ export function Aulas() {
                         </ListItem>
                 }
             </List >
+            <div className='button__aulasCriar'>
+                <Button onClick={handleOpen}>Criar</Button>
+            </div>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+                className="aulasEdit_modal"
+            >
+                <Box sx={style}>
+                    <form onSubmit={aulaCriarInput}>
+                        <Typography sx={{ marginLeft: '10px', marginRight: '10px' }}>
+                            Categoria:
+                        </Typography>
+                        <FormControl fullWidth onChange={changeForm} sx={{ backgroundColor: '#e9dac9' }}>
+                            <OutlinedInput name="categoria" size='small' />
+                        </FormControl>
+                        <Typography sx={{ marginLeft: '10px', marginRight: '10px' }}>
+                            Descricao:
+                        </Typography>
+                        <FormControl fullWidth onChange={changeForm} sx={{ backgroundColor: '#e9dac9' }}>
+                            <OutlinedInput name="descricao" size='small' />
+                        </FormControl>
+                        <Typography sx={{ marginLeft: '10px', marginRight: '10px' }}>
+                            Link:
+                        </Typography>
+                        <FormControl fullWidth onChange={changeForm} sx={{ backgroundColor: '#e9dac9' }}>
+                            <OutlinedInput name="link" size='small' />
+                        </FormControl>
+                        <Typography sx={{ marginLeft: '10px', marginRight: '10px' }}>
+                            Data:
+                        </Typography>
+                        <FormControl fullWidth onChange={changeForm} sx={{ backgroundColor: '#e9dac9' }}>
+                            <OutlinedInput name="data" size='small' />
+                        </FormControl>
+                        <Button onClick={handleClose}>
+                            Cancelar
+                        </Button>
+                        <Button type="submit">
+                            Criar
+                        </Button>
+                    </form>
+                </Box>
+            </Modal>
 
 
         </div >
